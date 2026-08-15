8849b5242d2e6b267da3c3a0acf17f70c48a17db
Karling23 <clalquinga@gmail.com>
2026-08-14 21:04:09 -0500

    Evidencias examen

--- areopuertos-rn/App.tsx
+++ areopuertos-rn/App.tsx
@@ -3,8 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
-import ServiceTypesScreen from "./src/screens/ServiceTypesScreen";
-import VehicleServicesScreen from "./src/screens/VehicleServicesScreen";
+//import ServiceTypesScreen from "./src/screens/ServiceTypesScreen";
+//import VehicleServicesScreen from "./src/screens/VehicleServicesScreen";
 
 import type { RootStackParamList } from "./src/types/navigation";
 
@@ -16,8 +16,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Menú" }} />
-        <Stack.Screen name="ServiceTypes" component={ServiceTypesScreen} options={{ title: "Service Types" }} />
-        <Stack.Screen name="VehicleServices" component={VehicleServicesScreen} options={{ title: "Vehicle Services" }} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
